b7b8f59ad985699493d0eb8a30af011bc021aa89
andy <q@mq.local>
2021-09-21 13:31:20 +0400

    fix

--- src/views/index.vue
+++ src/views/index.vue
@@ -32,7 +32,7 @@
                     <img src="../images/logo.png">
                 </h1>
                 <h2>
-                    <p>Welcome to your iView app!</p>
+                    <p>Welcome to your iView app! hahah</p>
                     <Button @click="handleStart">Start iView</Button>
                 </h2>
             </Col>
